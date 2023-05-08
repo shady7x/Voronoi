cdffcf755ffaa55d02a7a789cbb83319f011cd91
Oleg Komarov <Home@DESKTOP-4FDKMVP>
2023-05-08 23:50:04 +0300

    fixed convex hull

--- voronoi.cpp
+++ voronoi.cpp
@@ -2,57 +2,60 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <set>
+#include <cmath>
 #include "vulkan_engine.h"
 
 const double EPS = 1e-10;
 
-int fuzzy_compare(double a, double b)
-{
+int fuzzyCompare(double a, double b) {
 	if (abs(a - b) < EPS)
 		return 0;
 	return a < b ? -1 : 1;
 }
 
-class Point
-{
-public:
-	const double x, y;
-	Point(double _x, double _y) : x(_x), y(_y) {}
-	static int orientation(Point* a, Point* b, Point* c)
-	{
-		double s = (b->x - a->x) * (c->y - a->y) - (b->y - a->y) * (c->x - a->x);
-		return s < -EPS ? -1 : (s > EPS ? 1 : 0); // -1 правее a b, 0 на линии, 1 левее a b
-	}
+class Point {
+    public:
+        const double x, y;
+        Point(double x, double y) : x(x), y(y) {}
+        double distSqr(const Point& p) {
+            return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+        }
+        static int orientation(Point* a, Point* b, Point* c) {
+            double s = (b->x - a->x) * (c->y - a->y) - (b->y - a->y) * (c->x - a->x);
+            return s < -EPS ? -1 : (s > EPS ? 1 : 0); // -1 правее a b, 0 на линии, 1 левее a b
+        }
 };
 
-class PolyNode
-{
-public:
-	Point* p;
-	PolyNode* next = nullptr;
-	PolyNode* prev = nullptr;
-	PolyNode(Point* _p) : p(_p) {}
+class PolyNode {
+    public:
+        Point* p;
+        PolyNode* next = nullptr;
+        PolyNode* prev = nullptr;
+
+        static PolyNode* makeNode(Point* p) {
+            auto node = new PolyNode(p);
+            node->next = node->prev = node;
+            return node;
+        }
+
+    private:
+        PolyNode(Point* p) : p(p) {}
 };
 
-std::pair< PolyNode*, PolyNode* > find_right_chain(Point* p, PolyNode* right)
-{
-	//if (right == nullptr)
-
-	if (right->next == nullptr || right->prev == nullptr)
+std::pair< PolyNode*, PolyNode* > findRightChain(Point* p, PolyNode* right) {
+	if (right == right->next)
 		return std::make_pair(right, right);
 
-	if (right->next == right->prev)
-	{
+	if (right->next == right->prev) {
 		return Point::orientation(p, right->p, right->next->p) < 0
 			? std::make_pair(right->next, right)
 			: std::make_pair(right, right->next);
 	}
 
 	PolyNode* current = right;
-	std::pair< PolyNode*, PolyNode* > chain;
-
-	while (chain.first == nullptr || chain.second == nullptr)
-	{
+	std::pair< PolyNode*, PolyNode* > chain{ nullptr, nullptr };
+	while (chain.first == nullptr || chain.second == nullptr) {
 		int prev_orient = Point::orientation(p, current->p, current->prev->p);
 		int next_orient = Point::orientation(p, current->p, current->next->p);
 
@@ -61,58 +64,51 @@ std::pair< PolyNode*, PolyNode* > find_right_chain(Point* p, PolyNode* right)
 
 		if (chain.second == nullptr && prev_orient <= 0 && next_orient < 0)
 			chain.second = current;
+
+        current = current->next;
 	}
 
 	return chain;
 }
 
-PolyNode* merge(PolyNode* left, PolyNode* right)
-{
-	auto chain = find_right_chain(left->p, right);
-	PolyNode* l = left->next;
+PolyNode* merge(PolyNode* left, PolyNode* right) {
+	auto chain = findRightChain(left->p, right);
+	PolyNode* l = left->next == left ? nullptr : left->next;
 	PolyNode* r = chain.first;
 	std::vector< PolyNode* > m;
 	m.emplace_back(left);
-	while (l != nullptr || r != nullptr)
-	{
+	while (l != nullptr || r != nullptr) {
 		PolyNode* curr;
-		bool rside = (l == nullptr || Point::orientation(left->p, l->p, r->p) < 0);
-		if (rside)
-		{
+		bool rside = l == nullptr || (r != nullptr && Point::orientation(left->p, l->p, r->p) < 0);
+		if (rside) {
 			curr = r;
 			r = r == chain.second ? nullptr : r->next;
-		}
-		else
-		{
+		} else {
 			curr = l;
 			l = l->next == left ? nullptr : l->next;
 		}
 
-		while (m.size() > 2 && Point::orientation(m[m.size() - 2]->p, m[m.size() - 1]->p, curr->p) <= 0)
-		{
+		while (m.size() >= 2 && Point::orientation(m[m.size() - 2]->p, m[m.size() - 1]->p, curr->p) <= 0) {
 			m.pop_back();
 		}
+
+        m.emplace_back(curr);
 	}
 
-	if (m.size() > 2 && Point::orientation(m[m.size() - 2]->p, m[m.size() - 1]->p, m[0]->p) <= 0) // может быть только == 0
-	{
+	if (m.size() > 2 && Point::orientation(m[m.size() - 2]->p, m[m.size() - 1]->p, m[0]->p) <= 0) { // может быть только == 0
 		m.pop_back();
 	}
 
-	for (size_t i = 0; i < m.size(); ++i)
-	{
+	for (size_t i = 0; i < m.size(); ++i) {
 		m[i]->prev = m[i == 0 ? m.size() - 1 : i - 1];
 		m[i]->next = m[(i + 1) % m.size()];
 	}
-
 	return m[0]; // left
 }
 
-PolyNode* kirkpatrick(const std::vector< Point* >& points, size_t begin, size_t end)
-{
-	if (end - begin == 1)
-	{
-		return new PolyNode(points[begin]);
+PolyNode* kirkpatrick(const std::vector< Point* >& points, size_t begin, size_t end) {
+	if (end - begin == 1) {
+		return PolyNode::makeNode(points[begin]);
 	}
 
 	size_t mid = (begin + end) / 2;
@@ -124,7 +120,23 @@ PolyNode* kirkpatrick(const std::vector< Point* >& points, size_t begin, size_t 
 
 int main(int argc, char** argv)
 {
+	int n;
+	std::set < std::pair < int, int > > v;
+	std::vector < Point* > points;
+	int x, y;
 
+	std::cin >> n;
+	for (int i = 0; i < n; ++i) {
+		std::cin >> x >> y;
+		v.emplace(x, y);
+	}
+
+	for (const auto& it : v) {
+		points.emplace_back(new Point(it.first, it.second));
+	}
+
+	auto head = kirkpatrick(points, 0, points.size());
+	
 	VulkanEngine vulkanEngine;
 
     try {
@@ -133,39 +145,6 @@ int main(int argc, char** argv)
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-	/*int n;
-	std::vector < std::pair < double, double > > v;
-	std::vector < Point* > points;
-	double x, y;
-
-	std::cin >> n;
-	for (int i = 0; i < n; ++i)
-	{
-		std::cin >> x >> y;
-		v.emplace_back(x, y);
-	}
-
-	std::sort(v.begin(), v.end());
-
-	for (size_t i = 0; i < v.size(); ++i)
-	{
-		if (i == 0 || v[i] != v[i - 1])
-		{
-			points.emplace_back(new Point(v[i].first, v[i].second));
-		}
-	}
-
-	auto head = kirkpatrick(points, 0, points.size());
-	*/
-
-
-	
-	/*std::vector< std::string > extensions;
-	getAvailableVulkanExtensions(window, extensions);
-    
-	SDL_Surface* screen_surface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0, 255, 0));
-	SDL_UpdateWindowSurface(window);*/
 
 	return 0;
 }
